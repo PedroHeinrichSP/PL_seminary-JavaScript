d82e431dcef77d765e76d23c508e2b524e8bb151
HeinrichFeather <103895487+PedroHeinrichSP@users.noreply.github.com>
2023-05-23 20:58:06 -0300

    mutcha cosa

---
+++ timeline.js
@@ -0,0 +1,13 @@
+var data = [
+    {time: new Date(1993, 1,1), name: 'Marc Andreessen e Eric Bina, desenvolvem o Mosaic'}, 
+    {time: new Date(1994, 4,1), name: 'Criação da Netscape Comunications Corporations'},
+    {time: new Date(1994, 5,1), name: 'Criação do Netscape Navigator'},
+    {time: new Date(1995, 1,1), name: 'Netscape Navigator supera o Mosaic'},
+    {time: new Date(1995, 3,1), name: 'Brendan Eich entra para a Netscape'},
+    {time: new Date(1995, 5,1), name: 'Protótipo da linguagem Mocha'},
+    {time: new Date(1995, 5,2), name: 'Anúncio público de JAVA pela Sun Microsystems'},
+    {time: new Date(1995, 9,1), name: 'Disponibilização do Netscape Navigator 2.0 Beta'},
+    {time: new Date(1995, 10,1), name: 'Anúncio de JavaScript pela Sun e Netscape'},
+    {time: new Date(1996, 3,1), name: 'Lançamento do Netscape Navigator 2.0'},
+    {time: new Date(1996, 8,1), name: 'Lançamento do Netscape Navigator 3.0'},
+    ];
